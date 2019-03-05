b1e327210c1b5f57072a9ee7ed650039b5187711
LemkeBen <benjamin.lemke@student.tdsb.on.ca>
2019-03-04 21:32:42 -0500

    Initial commit

---
+++ src/com/company/Q10.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Q10 {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ src/com/company/Q7.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Q7 {
+    public static void main(String[] args) {
+
+    }
+}
